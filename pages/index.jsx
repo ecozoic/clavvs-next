1f1c6cfbdbbeb46d80c276a0a7c7447f9fa41afb
ecozoic <johnnn.mitchell@gmail.com>
2018-11-08 16:25:05 -0500

    initial next infrastructure

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  color: blue;
+`;
+
+const Index = () => <Wrapper>Hello world</Wrapper>;
+
+export default Index;
